038d0dcf015c5fb025316b7924a2ef116a82a824
Santiago Ordonana <santiago@cerebrosm.com>
2018-10-02 16:34:14 -0500

    Tarea Final

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,17 +13,14 @@ $(document).ready(function () {
   
     var database = firebase.database();
   
-    // Capture Button Click
     $("#addTrain").on("click", function (event) {
       event.preventDefault();
   
-      // Grabbed values from text boxes
       var trainName = $("#trainName").val().trim();
       var destination = $("#destination").val().trim();
       var firstTrain = $("#firstTrain").val().trim();
       var freq = $("#interval").val().trim();
   
-      // Code for handling the push
       database.ref().push({
         trainName: trainName,
         destination: destination,
@@ -33,7 +30,6 @@ $(document).ready(function () {
     });
   
   
-    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
     database.ref().on("child_added", function (childSnapshot) {
   
       var newTrain = childSnapshot.val().trainName;
@@ -41,26 +37,19 @@ $(document).ready(function () {
       var newFirstTrain = childSnapshot.val().firstTrain;
       var newFreq = childSnapshot.val().frequency;
   
-      // First Time (pushed back 1 year to make sure it comes before current time)
       var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
   
-      // Current Time
       var currentTime = moment();
   
-      // Difference between the times
       var diffTime = moment().diff(moment(startTimeConverted), "minutes");
   
-      // Time apart (remainder)
       var tRemainder = diffTime % newFreq;
   
-      // Minute(s) Until Train
       var tMinutesTillTrain = newFreq - tRemainder;
   
-      // Next Train
       var nextTrain = moment().add(tMinutesTillTrain, "minutes");
       var catchTrain = moment(nextTrain).format("HH:mm");
   
-      // Display On Page
       $("#all-display").append(
         ' <tr><td>' + newTrain +
         ' </td><td>' + newLocation +
@@ -68,14 +57,12 @@ $(document).ready(function () {
         ' </td><td>' + catchTrain +
         ' </td><td>' + tMinutesTillTrain + ' </td></tr>');
   
-      // Clear input fields
       $("#trainName, #destination, #firstTrain, #interval").val("");
       return false;
     },
-      //Handle the errors
       function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
       });
   
-  }); //end document ready
+  }); 
   
